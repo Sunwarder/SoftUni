8b1d09d17a713042ab464bd087512c9c63e255bd
Sunwarder <vbishurov@gmail.com>
2014-08-20 21:37:24 +0300

    Add

---
+++ Sunglasses/Sunglasses/Sunglasses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunglasses
+{
+    class Sunglasses
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                {
+                    if (i == 0 || i == n - 1)
+                    {
+                        WriteTopBottomPart(n);
+                    }
+                    else
+                    {
+                        WriteMiddlePart(n, i);
+                    }
+                }
+            }
+        }
+        private static void WriteTopBottomPart(int n)
+        {
+            string frame = new string('*', n);
+            string spaces = new string(' ', n);
+            
+                Console.WriteLine("{0}{1}{0}",frame,spaces,frame);
+  
+        }
+        private static void WriteMiddlePart(int n, int i)
+        {
+            string lens = new string('/', n - 2);
+            string bridge = new string(' ', n);
+            string middleFrame = string.Format("{0}{1}{0}", '*', lens);
+            if (i==n/2)
+            {
+                bridge = new string('|', n);
+            }
+            Console.WriteLine("{0}{1}{2}",middleFrame,bridge,middleFrame);
+        }
+    }
+}
---
+++ WeAllLoveBits/WeAllLoveBits/WeAllLoveBits.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeAllLoveBits
+{
+    class WeAllLoveBits
+    {
+        static void Main(string[] args)
+        {
+            int amount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < amount; i++)
+            {
+                long n = long.Parse(Console.ReadLine());
+                string num = Convert.ToString(n, 2);
+                long invertedBitsNumber = n;
+                for (int j = 0; j < num.Length; j++)
+                {
+                    invertedBitsNumber = invertedBitsNumber ^ (1 << j);
+                }
+                string newNumString = Reverse(num);
+                long reversedBitsNumber = Convert.ToInt64(newNumString, 2);
+                Console.WriteLine((n^invertedBitsNumber)&reversedBitsNumber);
+            }
+        }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
