2234389cac0eb84276a17acf087ff964c392b0f8
Velizar Bishurov <vbishurov@gmail.com>
2014-12-07 11:19:28 +0200

    exam prep 26.08.2014

---
+++ CSharpBasicsExam26August2014/01.TicTacToePower/TicTacToePower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToePower
+{
+    class TicTacToePower
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int firstValue = int.Parse(Console.ReadLine());
+
+            int index = y * 3 + x + 1;
+            int value = firstValue + index - 1;
+            Console.WriteLine((long)Math.Pow(value,index));
+        }
+    }
+}
---
+++ CSharpBasicsExam26August2014/02.JumpingSums/JumpingSums.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpingSums
+{
+    class JumpingSums
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split();
+            int[] numbers = new int[line.Length];
+            for (int i = 0; i < line.Length; i++)
+            {
+                numbers[i] = int.Parse(line[i]);
+            }
+            int numberOfJumps = int.Parse(Console.ReadLine());
+
+            int maxSum = int.MinValue;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int value = numbers[i];
+                int currentIndex = i;
+                int nextIndex = (currentIndex + value) % numbers.Length;
+                int currentSum = 0;
+
+                for (int j = 0; j <= numberOfJumps; j++)
+                {
+                    currentSum += value;
+                    value = numbers[nextIndex];
+                    currentIndex = nextIndex;
+                    nextIndex = (currentIndex + value) % numbers.Length;
+                }
+
+                if (currentSum>maxSum)
+                {
+                    maxSum = currentSum;
+                }
+            }
+            Console.WriteLine("max sum = {0}", maxSum);
+        }
+    }
+}
